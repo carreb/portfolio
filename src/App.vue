3315049a3c5598ebc64720584cd1bf1f61b0388d
carreb <caleb@domyjobfor.me>
2023-03-30 20:13:10 -0400

    add a page index for commissions

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@
            <p><a class="navbar" @click="this.$router.push('/projects')">projects <!--<font-awesome-icon icon="fa-solid fa-caret-down" />--></a></p> 
         </li>
         <li class="nav-item">
+          <p><a class="navbar" @click="this.$router.push('/comissions')">comissions</a></p>
+        </li>
+        <li class="nav-item">
           <p><a class="navbar" @click="this.$router.push('/contact')">contact</a></p>
         </li>
       </div>
