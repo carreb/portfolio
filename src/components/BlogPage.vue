b56fe6380fbaf2a18dd78334155c019b9b69a7ea
carreb <caleb@domyjobfor.me>
2023-05-02 00:13:20 -0400

    this SHOULD be the last fix

--- src/components/BlogPage.vue
+++ src/components/BlogPage.vue
@@ -14,7 +14,7 @@
     <div class="blog-container">
         <transition name="slide-in" mode="out-in">
             <div class="posts-flex-container">
-                <div id="blog-post" class="blog-post" v-for="(post, index) in sortedPosts" :key="post.slug + '_' + index" @click="this.$router.push('/blog/' + post.slug)">
+                <div id="blog-post" class="blog-post" v-for="(post, index) in reverseArray(sortedPosts)" :key="post.slug + '_' + index" @click="this.$router.push('/blog/' + post.slug)">
                     <article class="media">
                         <figure>
                             <img v-if="post.featured_image" :src="post.featured_image" class="featured-image">
@@ -113,6 +113,11 @@ const butter = Butter(process.env.VUE_APP_BUTTERCMS_KEY)
                     return categoryAmount
                 }
             },
+            reverseArray() {
+                return function(array) {
+                    return array.slice().reverse()
+                }
+            }
         }
     }
 </script>
